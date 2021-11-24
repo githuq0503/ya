5b5ddddea9a1bfac7addca9045656a51c3c7bfca
HannGanzz <92915874+HannGanzz@users.noreply.github.com>
2021-11-24 20:05:33 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -23,13 +23,11 @@ return `ℒ⃝👾 *B O T  S T A T I S T I C*
 ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
 
 ⛎ Api : api.alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
-♉ Github : zeeoneofc.github.io
-♊ Instagram : instagram.com/zeeoneofc
-♋ Tiktok : tiktok.com/@zeeoneofc
-♌ Telegram : t.me/zeeoneofc
-♎ Ch Telegram : t.me/zeeone_ofc
-♏ WhatsApp : bit.ly/zeeone_ofc
+♉ Github : github.com/HannGanzz
+♊ Instagram : instagram.com/mr.lucifer086
+♋ Tiktok : tiktok.com/@aelah28
+♌ Telegram : t.me/HannGanzzz
+♏ WhatsApp : bit.ly/HannGanz
 `}
 
 exports.infobot = infobot
